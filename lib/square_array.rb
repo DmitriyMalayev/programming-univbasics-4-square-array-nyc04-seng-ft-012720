79f2aa932c14c10e88e11bc56fcd437b8dd7b06a
Dmitriy Malayev <dmitriy.malayev@gmail.com>
2020-01-12 23:37:23 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,8 +2,8 @@ def square_array(array)
   counter = 0 
   array2 = []
   while array[counter] do 
-    array[counter]**2
-    array2.push(puts array[counter]) 
+    array2.push(array[counter]**2) 
     counter++
   end 
+  return array2 
 end
